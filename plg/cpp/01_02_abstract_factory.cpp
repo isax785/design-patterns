39ed8d3a3f5a4203b94d26a98857a4cf10ca60d3
i.stiaccini <i.stiaccini@frigel.com>
2024-09-04 10:24:47 +0200

    abstract factory C++

--- plg/cpp/01_02_abstract_factory.cpp
+++ plg/cpp/01_02_abstract_factory.cpp
@@ -1,7 +1,79 @@
 #include <iostream>
-
 using namespace std;
 
+struct Button {
+    virtual void click()=0;
+    // virtual void optional_method();
+};
+
+struct Checkbox {
+    virtual void check()=0;
+};
+
+struct WindowsButton: public Button {
+    void click() override{cout << "Windows Button clicked" << endl;}
+    };
+struct LinuxButton: public Button {
+    void click() override{cout << "Linux Button clicked" << endl;}
+    };
+struct MacButton: public Button {
+    void click() override{cout << "Mac Button clicked" << endl;}
+    };
+struct WindowsCheckbox: public Checkbox {
+    void check() override {cout << "Windows Checkbox checked" << endl;}
+    };
+struct LinuxCheckbox: public Checkbox {
+    void check() override {cout << "Linux Checkbox checked" << endl;}
+    };
+struct MacCheckbox: public Checkbox {
+    void check() override {cout << "Mac Checkbox checked" << endl;}
+    };
+
+struct GUIFactory {
+    virtual Button* create_button()=0;
+    virtual Checkbox* create_checkbox()=0;
+};
+
+struct WindowsFactory : public GUIFactory {
+    Button* create_button() override{return new WindowsButton;};
+    Checkbox* create_checkbox() override{return new WindowsCheckbox;};
+};
+
+struct LinuxFactory : public GUIFactory {
+    Button* create_button() override{return new LinuxButton;};
+    Checkbox* create_checkbox() override{return new LinuxCheckbox;};
+};
+
+struct MacFactory : public GUIFactory {
+    Button* create_button() override{return new MacButton;};
+    Checkbox* create_checkbox() override{return new MacCheckbox;};
+};
+
+// Client code
+void create_ui(GUIFactory* factory) {
+    Button* button = factory->create_button();
+    Checkbox* checkbox = factory->create_checkbox();
+    button->click();
+    checkbox->check();
+}
+
+enum OS {WINDOWS, LINUX, MAC};
+
 int main() {
+    OS os = MAC;
+    GUIFactory* factory;
+    switch (os){
+        case WINDOWS:
+            factory = new WindowsFactory();
+            break;
+        case LINUX:
+            factory = new LinuxFactory();
+            break;
+        case MAC:
+            factory = new MacFactory();
+            break;        
+    }
+    create_ui(factory);
+
     return 0;
 }
