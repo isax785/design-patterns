84188fff36519c0b298b43084ebbcb4cc416b5bc
i.stiaccini <i.stiaccini@frigel.com>
2024-10-22 09:38:43 +0200

    flyweight C++ completed

--- plg/cpp/02_06_flyweight.cpp
+++ plg/cpp/02_06_flyweight.cpp
@@ -1,16 +1,54 @@
 #include <iostream>
 #include <string>
+#include <map>
 #include <stdio.h>
 
 using namespace std;
+
+const int FS = 10;  // Factory Size
 
 struct Circle {
     const string m_color;
     Circle(const string color) : m_color(color) {
         printf("Generated %s circle\n", m_color.c_str());
     }
+
+    void draw(float x, float y, float radius) {
+        printf("Drawing a %s circle at (%f, %f) with radius %f", m_color.c_str(), x, y, radius);
+    }
 };
 
+bool compare_strings(string& s1, string& s2) {
+    if (s1.size() != s2.size()) return false;
+    for(int i=0; i<s1.size(); i++) {
+        if (s1[i] != s2[i]) return false;
+    }
+    return true;
+};
+
+struct CircleFactory {
+    map<string, Circle*> circles;
+
+    Circle* get_circle(const string color) {
+        if (circles.find(color) != circles.end()) {
+            cout << color << " circle yet created" << endl;
+            return circles[color];
+        }
+        else {
+            circles[color] = new Circle(color);
+            return circles[color];
+        }
+    }
+};
+
+
 int main() {
-    Circle c = Circle("red");
+    const int N=5;
+    CircleFactory factory = CircleFactory();
+    const string colors[N] = {"red", "green", "blue", "red", "blue"};
+    for (int i=0; i<N; i++) {
+        cout << "DBG-m: " << colors[i] << endl; // DBG
+        Circle *circle = factory.get_circle(colors[i]);
+        circle->draw(12.3, 14.4, 1.2343);
+    }
 }
--- plg/cpp/02_06_flyweight_char.cpp
+++ plg/cpp/02_06_flyweight_char.cpp
@@ -25,18 +25,18 @@ struct CircleP {
 
 struct CircleFactory {
     Circle* circles[FACTORY_SIZE];
-    const char* colors[FACTORY_SIZE];
+    const char* colors[FACTORY_SIZE] = {"-"};
     int counter = 0;
 
     Circle* get_circle(const char* color) { 
         printf("DBG: get_circle color %s\n", color);
         for (int i=0; i<FACTORY_SIZE; i++) {
-            cout << "DBG: looking for " << color << endl; // DEBUG
-            if (strcmp(color, colors[i])) 
-        //     {
-                printf("DBG: color %s yet created", color); // DEBUG
-        // //         return circles[i];
-        //     }
+            // printf("DBG: searching %d %s %s\n", i, colors[i], color); // DEBUG
+
+            if (strcmp(color, colors[i])) {
+                printf("DBG: color %s yet created\n", color); // DEBUG
+                return circles[i];
+            }
         }
         cout << "DBG: Creating " << color << endl; // DEBUG
         printf("DBG: Counter %d\n", counter);
@@ -56,14 +56,13 @@ int main() {
     const char* colors[N] = {"red", "green", "blue", "red", "blue"};
     CircleFactory factory = CircleFactory();
     for (int i=0; i<N; i++) {
-        cout << "DBG: " << colors[i] << endl; // DBG
+        cout << "DBG-m: " << colors[i] << endl; // DBG
         // CircleP c = CircleP(colors[i]); // This works!!
         // c.draw(2.3, 4.5, 6.789);
         Circle *circle = factory.get_circle(colors[i]);
-        // circle->draw(12.3, 14.4, 1.2343);
+        circle->draw(12.3, 14.4, 1.2343);
     }
     cout << strcmp("red", "red") << endl;
     // Circle* circ = factory.circles[1];
     // cout << circ->m_color  << endl;
 }
-
